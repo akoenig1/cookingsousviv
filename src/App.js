2f0117b583bbfcfb4b7dd96b50871968c1058f7c
Alex Koenig <alex.koenig4@gmail.com>
2021-03-15 22:20:06 -0400

    Refactor src and context files

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Routes from './Routes';
+import Auth from './components/Auth';
+import { AuthContext } from './context/auth-context';
+//import queryString from "query-string"
+import './styles/styles.css';
+
+function App() {
+  const [userId, setUserId] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  //const [error, setError] = useState(null)
+  const [token, setToken] = useState(false);
+
+  const login = useCallback((uid, token, admin, expirationDate) => {
+    setToken(token);
+    setUserId(uid);
+    setIsAdmin(admin);
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60 * 24);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({ 
+        token: token, 
+        userId: uid, 
+        admin: admin,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
+  }, []);
+  
+  const logout = useCallback(() => {
+    setToken(null);
+    setUserId(null);
+    setIsAdmin(null);
+    localStorage.removeItem('userData');
+  }, []);
+
+  useEffect( () => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if(
+      storedData && 
+      storedData.token && 
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId , storedData.token, storedData.admin, new Date(storedData.expiration))
+    }
+  }, [login])
+
+  return (
+    <div className="app">
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: !!token,
+          token: token,
+          userId: userId,
+          isAdmin: isAdmin,
+          login: login,
+          logout: logout,
+        }}
+      >
+        <Navbar />
+        <Auth></Auth>
+        <Routes/>
+      </AuthContext.Provider>
+    </div>
+  );
+}
+
+export default App;
--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 
 export const AuthContext = createContext({
-    isLoggedIn: false,
-    userId: null,
-    token: null,
-    isAdmin: false,
-    login: () => {},
-    logout: () => {}
+  isLoggedIn: false,
+  userId: null,
+  token: null,
+  isAdmin: false,
+  login: () => {},
+  logout: () => {},
 });
